11e6bbce4bef625558a3132893b3a7ca4c9f5c29
tuyentac95 <tuyentac95@gmail.com>
2021-09-30 20:21:56 +0700

    init project

---
+++ src/main/java/com/market/gold/GoldApplication.java
@@ -0,0 +1,13 @@
+package com.market.gold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/gold/GoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.gold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
