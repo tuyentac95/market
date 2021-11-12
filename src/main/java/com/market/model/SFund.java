dc210fdcf75c992d78c8f2164c70f2e8c3215e83
tuyentac95 <tuyentac95@gmail.com>
2021-11-12 20:56:36 +0700

    handle save funds

---
+++ src/main/java/com/market/model/SFund.java
@@ -0,0 +1,17 @@
+package com.market.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SFund {
+    private String name;
+    private String code;
+    private LocalDate date;
+    private Double nav;
+}
--- src/main/java/com/market/serivce/FundService.java
+++ src/main/java/com/market/serivce/FundService.java
@@ -2,7 +2,9 @@ package com.market.serivce;
 
 import com.market.factory.FundFactory;
 import com.market.model.Fund;
+import com.market.model.SFund;
 import com.market.model.FundParameter;
+import com.market.repository.FundRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +12,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.regex.Matcher;
+import java.util.stream.Collectors;
 
 @Service
 public class FundService {
@@ -18,7 +22,10 @@ public class FundService {
     @Autowired
     private UrlService urlService;
 
-    public Fund fetchNav(String code) throws IOException {
+    @Autowired
+    private FundRepository repository;
+
+    public SFund fetchNav(String code) throws IOException {
         FundParameter parameter = FundFactory.getFund(code);
         System.out.println("[INFO] Start fetching URL");
         URL url = new URL(parameter.getBaseURL());
@@ -29,12 +36,28 @@ public class FundService {
         Matcher matcher = parameter.getPattern().matcher(content);
 
         while (matcher.find()) {
-            return new Fund(
+            return new SFund(
                     parameter.getName(),
                     parameter.getCode(),
                     LocalDate.parse(matcher.group(parameter.getDateMatchGroup()), DateTimeFormatter.ofPattern("dd/MM/yyyy")),
                     Double.parseDouble(matcher.group(parameter.getNavMatchGroup()).replaceAll(",", ".")));
         }
         return null;
+    }
+
+    public void saveFunds(List<Fund> funds, String date) {
+        List<Fund> oldList = repository.findAll();
+        List<Fund> newList = funds.stream().map(fund -> {
+            String code = fund.getCode();
+            Fund f = oldList.stream().filter(f1 -> code.equals(f1.getCode())).findFirst().orElse(fund);
+            f.setNav(fund.getNav());
+            f.setDate(LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            return f;
+        }).collect(Collectors.toList());
+        repository.saveAll(newList);
+    }
+
+    public Fund findFund(String code) {
+        return repository.findByCode(code).orElse(null);
     }
 }
